84b4eb13948f928e0b3d8d9732102f7f29361e16
Vishal Sharma <sharma.vishal2799@gmail.com>
2022-01-20 07:59:28 +0530

    created 404 route

--- src/routes/NoMatch/NoMatch.js
+++ src/routes/NoMatch/NoMatch.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import bgImg from '../../assets/images/bg-43.png'
+import Button from '../../components/UI/Button/Button'
+import classes from './NoMatch.module.css'
+import { useNavigate } from 'react-router-dom'
+const NoMatch = () => {
+    let navigate = useNavigate();
 
-const noMatch = () => {
     return (
         <div>
-            NoMatch
+            <div className={classes.NoMatch}>
+            <img src={bgImg} />
+            <h1>Oops! It looks like you're lost.</h1>
+            <p>The page you're looking for isn't available. Try to search again or use the go to.</p>
+            <Button variant="contained" onClick={() => navigate('/')}>Home Page</Button>
+        </div>
         </div>
     )
 }
 
-export default noMatch
+export default NoMatch
